44b206bf1d939c81450642343addeea03a21cbbf
PravinBiradar95 <105072152+PravinBiradar95@users.noreply.github.com>
2022-07-20 23:28:11 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,15 @@
 
 
 // export class using module.exports
+import {Shape} from "./Amazon"
+
+class Circle extends Shape{
+    calculateArea(){
+
+    };
+}
+
+const circle1 = new Circle;
+console.log(Circle.color)
+
+module.exports.Circle = Circle;
